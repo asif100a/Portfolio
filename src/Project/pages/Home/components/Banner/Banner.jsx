d0ed590cd45b0f596c0abba5a3cbf32a3d9426b1
N.M. Asif Sheikh <asifsheikhrrr@gmail.com>
2024-09-20 01:18:28 +0600

    Infinite Rotation added

--- src/Project/pages/Home/components/Banner/Banner.jsx
+++ src/Project/pages/Home/components/Banner/Banner.jsx
@@ -1,18 +1,24 @@
+import { SlSettings } from "react-icons/sl";
 import codingLaptop from "../../../../../assets/Coding-laptop.png";
-import code from "/code.png";
 import DownloadResume from "../downloadResume/DownloadResume";
 import "./banner.css";
 
 const Banner = () => {
     return (
-        <div className="w-auto h-screen ">
-            <div className="w-auto h-screen bg-black bg-opacity-40">
-                <div className="flex md:flex-row flex-col justify-around items-start lg:pt-32 pt-10">
+        <div className="w-auto h-screen banner-background">
+            <div className="w-auto h-full bg-black bg-opacity-40">
+                <div className="py-12 px-36 flex items-center justify- gap-16">
+                    <SlSettings className="w-20 h-20 text-[#c966f4] rotate-animation" />
+                    <h1 className="text-6xl font-semibold space-x-6">
+                        <span>Welcome to</span> <span>N.M. Asif Sheikh</span>
+                    </h1>
+                </div>
+                <div className="flex md:flex-row flex-col justify-center items-center gap-6 ">
                     {/* Code Shower Laptop */}
                     <div className="md:block hidden mt-3 hover:cursor-not-allowed relative w-fit h-fit">
                         <img src={codingLaptop} alt="N.M. Asif Sheikh" className="lg:h-80 md:h-56 w-auto" />
 
-                        {/* Code gif */}
+                        {/* gif */}
                         <div className="absolute top-[26px] lg:top-[38px] left-[57px] lg:left-[86px] flex">
                             <div className="bg-[#282828] w-[28px] lg:w-[42px] xl:w-[46px] h-[138px] lg:h-[195px]"></div>
                             <iframe
