c63e0f0289cb2dd1685cd315528845461254fa02
N.M. Asif Sheikh <asifsheikhrrr@gmail.com>
2024-09-25 18:25:47 +0600

    Contact Me-navbar button style changed

--- src/Project/sharedFile/navber/Navber.jsx
+++ src/Project/sharedFile/navber/Navber.jsx
@@ -24,7 +24,7 @@ const Navber = () => {
             title: 'Contact'
         },
     ];
-    
+
     const navItems = <>
         {
             items.map((item, i) => (
@@ -34,38 +34,42 @@ const Navber = () => {
     </>;
 
     return (
-        <div className="navbar bg-[#0d1117] gradient-border-bottom">
-            <div className="lg:navbar-start flex-row-reverse lg:flex-row lg:w-1/2 w-full">
-                <div className="dropdown flex flex-row-reverse justify-between lg:inline-block lg:w-auto w-full">
-                    <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
-                        <svg
-                            xmlns="http://www.w3.org/2000/svg"
-                            className="h-5 w-5 text-white"
-                            fill="none"
-                            viewBox="0 0 24 24"
-                            stroke="currentColor">
-                            <path
-                                strokeLinecap="round"
-                                strokeLinejoin="round"
-                                strokeWidth="2"
-                                d="M4 6h16M4 12h8m-8 6h16" />
-                        </svg>
-                    </div>
+        <header className="w-full h-full bg-[#0d1117] px-28">
+            <div className="navbar gradient-border-nav px-8">
+                <div className="lg:navbar-start flex-row-reverse lg:flex-row lg:w-1/2 w-full">
+                    <nav className="dropdown flex flex-row-reverse justify-between lg:inline-block lg:w-auto w-full">
+                        <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
+                            <svg
+                                xmlns="http://www.w3.org/2000/svg"
+                                className="h-5 w-5 text-white"
+                                fill="none"
+                                viewBox="0 0 24 24"
+                                stroke="currentColor">
+                                <path
+                                    strokeLinecap="round"
+                                    strokeLinejoin="round"
+                                    strokeWidth="2"
+                                    d="M4 6h16M4 12h8m-8 6h16" />
+                            </svg>
+                        </div>
 
-                    <ul
-                        tabIndex={0}
-                        className="menu menu-sm dropdown-content bg-base-100 rounded-box z-[1] mt-3 w-52 p-2 shadow">
-                        {navItems}
-                    </ul>
+                        <ul
+                            tabIndex={0}
+                            className="menu menu-sm dropdown-content bg-base-100 rounded-box z-[1] mt-3 w-52 p-2 shadow">
+                            {navItems}
+                        </ul>
 
-                    <p className="sm:text-3xl text-2xl font-semibold gradient-text flex justify-center items-center">Front-End Developer</p>
+                        <p className="sm:text-3xl text-2xl font-semibold gradient-text flex justify-center items-center">Front-End Developer</p>
+                    </nav>
+                </div>
+
+                <div className="navbar-end hidden lg:flex">
+                    <Link to="contact" smooth={true} duration={600}>
+                        <button className="btn btn-sm text-lg font-medium px-6 h-10 min-h-10 bg-white hover:bg-white shadow-none border-none bg-gradient-to-r from-[#fb7185] to-[#eea75b] text-white hover:bg-gradient-to-r hover:from-[#fb7185] hover:to-[#eea75b] rounded-3xl">Contact Me</button>
+                    </Link>
                 </div>
             </div>
-            
-            <div className="navbar-end hidden lg:flex">
-                <Link to="contact" smooth={true} duration={600} className="btn btn-sm rounded-none bg-white hover:bg-white shadow-none border-none bg-gradient-to-r from-[#fb7185] to-[#eea75b] text-white hover:bg-gradient-to-r hover:from-[#fb7185] hover:to-[#eea75b]">Contact me</Link>
-            </div>
-        </div>
+        </header>
     );
 };
 
